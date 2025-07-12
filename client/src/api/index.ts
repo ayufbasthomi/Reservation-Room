998ff5f46d2f1d5298ac81f5d87d5e0acf16f772
ayufbasthomi <ayufbasthomi@gmail.com>
2025-07-12 11:36:42 +0700

    fixed issue

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,12 +1,12 @@
-const BASE_URL = 'http://localhost:5000/api';
+const API = import.meta.env.VITE_API_BASE_URL;
 
 export const fetchRooms = async () => {
-  const res = await fetch(`${BASE_URL}/rooms`);
+  const res = await fetch(`${API}/rooms`);
   return res.json();
 };
 
 export const reserveRoom = async (data: any) => {
-  const res = await fetch(`${BASE_URL}/reservations`, {
+  const res = await fetch(`${API}/reservations`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data),
